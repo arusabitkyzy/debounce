879480439f8ca23a09325b6026cb5f89515e277a
Aryzhan Sabitkyzy <aryzhan@nba-032-a1b-kz.vention.local>
2024-06-10 11:27:58 +0500

    fix: result bugs are fixed

--- script.js
+++ script.js
@@ -2,31 +2,46 @@ const API_KEY = 'a8e172cb';
 const input = document.querySelector('.input');
 const resultedFilms = document.querySelector('.films');
 
-const handleChange = debounce(fetchData, 2000);
+const handleChange = debounce(getMovie, 1000);
 
-async function fetchData() {
-  if (input.value == '') {
-    resultedFilms.innerHTML = '';
-    return;
-  }
-
+async function fetchData(movie) {
   let response = await fetch(
-    `http://www.omdbapi.com/?apikey=${API_KEY}&t=${encodeURIComponent(
-      input.value
-    )}`
+    `http://www.omdbapi.com/?apikey=${API_KEY}&t=${movie}`
   );
   let result = await response.json();
+  if (result.Title) {
+    showResults(result);
+  }
+}
 
-  const film = document.createElement('li');
-  film.innerHTML = `${result.Title}`;
-  resultedFilms.appendChild(film);
+function getMovie() {
+  const movieInput = input.value;
+  const movieEncoded = encodeURIComponent(input.value);
+  if (movieInput.length <= 0) {
+    resultedFilms.innerHTML = '';
+    resultedFilms.innerHTML = `<h3>Please enter a film</h3>`;
+  } else {
+    console.log(movieEncoded);
+    fetchData(movieEncoded);
+  }
+}
+
+function showResults(result) {
+  resultedFilms.innerHTML = '';
+  resultedFilms.innerHTML = `
+   <li>
+     <div>
+     ${result.Title}
+     ${result.Year}
+     </div>
+     </li>
+  `;
 }
 
 function debounce(fn, delay) {
-  let timer;
-  clearTimeout(timer);
-
-  return () => {
+  return function () {
+    let timer;
+    clearTimeout(timer);
     timer = setTimeout(fn, delay);
   };
 }
